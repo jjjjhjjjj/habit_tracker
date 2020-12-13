530d65d51d2c8d90a809e1146ac22d551d6003d4
hj <jjjjhjjjj123@gmail.com>
2020-12-13 22:34:39 +0900

    add habit tracker

---
+++ src/components/habitAddForm.jsx
@@ -0,0 +1,29 @@
+import React, { memo } from 'react';
+
+const HabitAddForm = memo((props) => {
+  const formRef = React.createRef();
+  const inputRef = React.createRef();
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const name = inputRef.current.value;
+    name && props.onAdd(name);
+    //this.inputRef.current.value = "";
+    formRef.current.reset();
+  }
+  return (
+    <form ref={formRef} onSubmit={onSubmit} className="add-form">
+      <input 
+        ref={inputRef} 
+        type="text" 
+        className="add-input"
+         placeholder="Habit" 
+      />
+      <button type="submit" className="add-button">
+        Add
+      </button>
+    </form>
+  );
+});
+
+export default HabitAddForm;
+
---
+++ src/components/habits.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import HabitAddForm from './habitAddForm';
+import Habit from './habit';
+
+class Habits extends Component {
+  
+  handleIncrement = (habit) => {
+    this.props.onIncrement(habit);
+  }
+
+  handleDecrement = (habit) => {
+    this.props.onDecrement(habit);
+  }
+
+  handleDelete = (habit) => {
+    this.props.onDelete(habit);
+  }
+  
+  hadleAdd = (name) => {
+    this.props.onAdd(name);
+  }
+
+  render() {
+    return (
+      <div> 
+        <HabitAddForm onAdd={this.hadleAdd}/>
+        <ul>
+          {this.props.habits.map(habit => 
+            <Habit 
+              key={habit.id} 
+              habit={habit}
+              onIncrement={this.handleIncrement} 
+              onDecrement={this.handleDecrement} 
+              onDelete={this.handleDelete}
+            />
+          )}
+        </ul>
+        <button type="button" className="habits-reset" onClick={this.props.onReset}>
+          Reset All
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Habits;
